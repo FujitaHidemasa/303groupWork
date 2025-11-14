0498927db08dad9362cd1546ddbaf57fb42d90f4
3030838 <3030838@30308-38>
2025-11-14 14:53:57 +0900

    谷口 MapperからServiceを注入に変更

--- src/main/java/com/example/voidr/controller/admin/AdminItemController.java
+++ src/main/java/com/example/voidr/controller/admin/AdminItemController.java
@@ -8,26 +8,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.voidr.entity.Item;
-import com.example.voidr.repository.ItemCategoryMapper;
-import com.example.voidr.repository.ItemMapper;
+import com.example.voidr.service.ItemService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/admin/items")  // 商品管理用URL
+@RequiredArgsConstructor        // 11/14追加（谷口）コンストラクタ注入（Lombok）
 public class AdminItemController {
 	
-	 private final ItemMapper itemMapper;
-	 private final ItemCategoryMapper itemCategoryMapper;
+    /** 11/13修正（谷口）Mapper ではなく Service を注入 */
+    private final ItemService itemService;
 
-	 public AdminItemController(ItemMapper itemMapper, ItemCategoryMapper itemCategoryMapper) {
-	        this.itemMapper = itemMapper;
-	        this.itemCategoryMapper = itemCategoryMapper;
-	    }
-
+	 /** 商品管理画面（一覧） */
     @GetMapping
     public String showItems(Model model) {
     	
     	//データベースから全商品を取得
-    	List<Item> items = itemMapper.selectAll();
+    	List<Item> items = itemService.getAllItems(); // Service経由に変更（谷口）
     	
     	// 取得した商品をセット
         model.addAttribute("items", items);
@@ -35,6 +33,4 @@ public class AdminItemController {
         return "admin/items";  // resources/templates/admin/items.html を表示
     
     }
-    
- 
 }
