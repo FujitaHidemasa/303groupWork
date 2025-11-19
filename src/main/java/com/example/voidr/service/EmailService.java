5f67eab6ab49164d665eefcc4ebd9ce13e228cea
3030838 <3030838@30308-38>
2025-11-19 10:13:28 +0900

    追加：購入確定送信メール機能

--- src/main/java/com/example/voidr/service/EmailService.java
+++ src/main/java/com/example/voidr/service/EmailService.java
@@ -1,9 +1,14 @@
 package com.example.voidr.service;
 
+import com.example.voidr.dto.PurchaseReceiptDto;
+
 public interface EmailService 
 {
 	void sendVerificationCode(String toEmail, String code);
 	
-	// 11/13 ★追加: パスワード再設定PIN送信用
+	// パスワード再設定PIN送信用
 	void sendPasswordResetPin(String toEmail, String pin);
+	
+	// 購入完了メール送信用
+	void sendPurchaseReceipt(String toEmail, PurchaseReceiptDto dto);
 }
